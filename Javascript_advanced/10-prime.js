940233aa14a328e42cc6e1b33aaf20b8c3e07fa5
OwenFernandez9 <9357@holbertonstudents.com>
2025-07-17 14:49:26 -0300

    prime 2

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,30 @@
+function countPrimeNumbers() {
+    let count = 0;
+
+    for (let num = 2; num <= 100; num++) {
+        let isPrime = true;
+
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+const t0 = performance.now();
+
+for (let i = 0; i < 100; i++) {
+    countPrimeNumbers();
+}
+
+const t1 = performance.now();
+
+console.log(`Execution time of calculating prime numbers 100 times was ${t1 - t0} milliseconds.`);
